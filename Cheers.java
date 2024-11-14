64bb53985105a312dc57ae1f8dacebc577aed5f9
raaedj <raed.agbaria@post.runi.ac.il>
2024-11-14 21:01:12 +0200

    test4

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,22 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args){
+        String name = args[0].toUpperCase();
+        int repeat = Integer.parseInt(args[1]);
+        String chars = "AEFHILMNORSX";
+        for(int i = 0; i < name.length(); i++){
+            if(chars.indexOf(name.charAt(i)) >= 0){
+                   System.out.println("Give me an " + name.charAt(i) + ": " + 
+                                      name.charAt(i) + "!");
+            }else{
+                System.out.println("Give me a " + name.charAt(i) + ": " + 
+                                      name.charAt(i) + "!");
+            }
         }
+        System.out.println("What does that spell?");
+        for(int i = 0; i < repeat; i++){
+            System.out.println(name + "!!!");
+        }
+
+    }
 }
